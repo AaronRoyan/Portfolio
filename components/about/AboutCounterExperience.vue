21e616e1f4e655f3167786fe962264de96a31be3
Aaron Royan <aaron@loyalytics.in>
2022-10-26 16:09:26 +0530

    1st push

---
+++ components/about/AboutCounterExperience.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  data: () => {
+    return {
+      // @todo
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="mt-20 sm:mt-20  bg-primary-light dark:bg-ternary-dark shadow-sm   ">
+    <div
+      class="
+        font-general-regular
+        container
+        mx-auto
+        py-20
+        block
+        sm:flex sm:justify-center sm:items-center gap-96 
+        text-center
+      "
+    >
+      <div class="mb-20 sm:mb-0">
+        <span
+          class="
+            font-general-medium
+            text-4xl
+            font-bold
+            text-secondary-dark
+            dark:text-secondary-light
+            mb-2
+          "
+          ><img
+            v-if="this.$colorMode.value == 'dark'"
+            src="~/static/conti.svg"
+            alt="Developer Dark"/>
+        </span
+        >
+        <!-- <span class="block text-md text-ternary-dark dark:text-ternary-light"
+          >1+</span
+        > -->
+      </div>
+      
+      <!-- <div class="mb-20 sm:mb-0">
+        <span
+          class="
+            font-general-medium
+            text-4xl
+            font-bold
+            text-secondary-dark
+            dark:text-secondary-light
+            mb-2
+          "
+          >21</span
+        >
+        <span class="block text-md text-ternary-dark dark:text-ternary-light"
+          >Projetos concluídos</span
+        >
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ pages/contact.vue
@@ -0,0 +1,49 @@
+<script>
+import feather from "feather-icons";
+
+export default {
+  scrollToTop: true,
+  data: () => {
+    return {
+      contacts: [
+        {
+          id: 1,
+          name: "India",
+          icon: "map-pin",
+        },
+        {
+          id: 2,
+          name: "royanjason@gmail.com",
+          icon: "mail",
+        },
+        {
+          id: 3,
+          name: "+91 99020 39107",
+          icon: "phone",
+        },
+      ],
+    };
+  },
+  mounted() {
+    feather.replace();
+  },
+  updated() {
+    feather.replace();
+  },
+};
+</script>
+
+<template>
+  <div
+    class="
+      container
+      mx-auto
+      flex flex-col-reverse
+      md:flex-row md:py-10 md:mt-20
+    "
+  >
+    <ContactForm />
+
+    <ContactDetails :contacts="contacts" />
+  </div>
+</template>
